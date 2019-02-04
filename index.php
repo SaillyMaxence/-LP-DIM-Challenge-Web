53eba1612b28dc07160d0befc6be4d2c22f519ca
maxence.sailly <maxencesai@gmail.com>
2019-02-04 11:56:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Challenge web</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style/style.css" />
+    <link rel="stylesheet" type="text/css" href="libs\font-awesome\css\font-awesome.min.css">
+</head>
+<body>
+    <?php
+
+        // vérif de la connexion a faire
+
+        // si connexion afficher index 
+
+        // Sinon affichage module connexion.
+    ?>
+    
+    <script src="libs/jquery.js"></script>
+    <script src="index.js"></script>       
+</body>
+</html>
