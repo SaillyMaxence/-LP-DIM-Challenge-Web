ad6e281f35c930b401108a48d69befb2c2ebe554
Julien Bruguet <lineproex@gmail.com>
2019-02-05 14:28:12 +0100

    Fix database connection

--- includes/database_inc.php
+++ includes/database_inc.php
@@ -5,9 +5,10 @@ $login = "root";
 $pwd = "";
 
 try {
-    $db = new PDO('mysql:host='.$host.';dbname='.$dbname, '$login', '$pwd');
+    $db = new PDO('mysql:host='.$host.';dbname='.$dbname, $login, $pwd);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     $e->getMessage();
+    echo $e;
 }
 ?>
