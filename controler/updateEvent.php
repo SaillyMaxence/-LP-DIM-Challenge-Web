67cf05335f7b9e1975b97f2b251c1aa5aca9a36b
Julien Bruguet <lineproex@gmail.com>
2019-02-05 15:44:09 +0100

    Corrections dans les add/delete/updateEvent
    Pas besoin de vérifier si un bouton est pressé

--- controler/updateEvent.php
+++ controler/updateEvent.php
@@ -1,26 +1,23 @@
 <?php
 require("../includes/database_inc.php");
 
-// Checking is the update button was pressed
-if(isset($_POST["update"])) {
-	// Grabbing data from the form
-	$id = $_POST["id"];
-	$title = $_POST["title"];
-	$message = $_POST["message"];
-	$start = $_POST["start"];
-	$end = $_POST["end"];
-	try {
-		// Preparing the update request
-    	$request = $db->prepare("UPDATE Events SET EventTitre = :title, EventMessage = :message, EventDateDebut = :start, EventDateFin = :end WHERE EventId = :id");
-    	// Binding values
-    	$request->bindValue(":id", $id);
-		$request->bindValue(":title", $title);
-		$request->bindValue(":message", $message);
-		$request->bindValue(":start", $start);
-		$request->bindValue(":end", $end);
-		// Firing the request
-		$request->execute();
-	} catch (Exception $e) { // If there was an error while updating
-	    throw "Error when editing: ".$e; // Display it
-	}
+// Grabbing data from the form
+$id = $_POST["id"];
+$title = $_POST["title"];
+$message = $_POST["message"];
+$start = $_POST["start"];
+$end = $_POST["end"];
+try {
+	// Preparing the update request
+   	$request = $db->prepare("UPDATE Events SET EventTitre = :title, EventMessage = :message, EventDateDebut = :start, EventDateFin = :end WHERE EventId = :id");
+   	// Binding values
+ 	$request->bindValue(":id", $id);
+	$request->bindValue(":title", $title);
+	$request->bindValue(":message", $message);
+	$request->bindValue(":start", $start);
+	$request->bindValue(":end", $end);
+	// Firing the request
+	$request->execute();
+} catch (Exception $e) { // If there was an error while updating
+    throw "Error when editing: ".$e; // Display it
 }
