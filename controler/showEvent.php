d6667e652c17d003b0929ee34442088db084e5f2
Julien Bruguet <lineproex@gmail.com>
2019-02-05 14:04:03 +0100

    Added showEvent

---
+++ controler/showEvent.php
@@ -0,0 +1,13 @@
+<?php
+require("../includes/database_inc.php");
+
+// Preparing the request to grab everything from the event table
+$request = $db->prepare("SELECT * FROM Events");
+// Firing the request
+$request->execute();
+
+// Fetching data from the request
+$response = $request->fetchAll(PDO::FETCH_ASSOC);
+
+// Returning everything in json format
+echo json_encode($response);
