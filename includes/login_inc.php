8e2bf193db573787dcfb195b1a2c7c84cac6148a
Florient <flo.roku@gmail.com>
2019-02-05 13:38:33 +0100

    login-ok

--- includes/login_inc.php
+++ includes/login_inc.php
@@ -5,7 +5,7 @@
         <input type="text" placeholder="Nom d'utilisateur" id="user" name="utilisateur" required>
 
         <label for="password">Mot de passe</label>
-        <input type="password" placeholder="mot de passe" id="password" name="password" required>
+        <input type="password" placeholder="Mot de passe" id="password" name="password" required>
         <div id="alert" >
       
         <p><span id="alertLabel"></span><span id="closeAlert" class="fa fa-close"></p></p>
