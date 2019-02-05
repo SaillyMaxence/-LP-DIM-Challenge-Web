5e36a44d1d022bc603d7c2cc443ef778ca3a2af9
Florient <flo.roku@gmail.com>
2019-02-05 13:58:02 +0100

    page

---
+++ pages/accueil.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Challenge web</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/style.css" />
+    <link rel="stylesheet" type="text/css" href="libs/datatable/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="libs\font-awesome\css\font-awesome.min.css">
+</head>
+<body>
+<nav><ul>
+    <a href="">
+        <li>Affichage</li>
+    </a>
+    <a href="">
+        <li>Evénements</li>
+    </a>
+    <a href="">
+        <li>Deconnection</li>
+    </a>
+</ul></nav>
+<div  class="container">
+</div>
+    <script src="libs/jquery.js"></script>
+    <script type="text/javascript" src="libs/datatable/datatables.min.js"></script>
+    <script src="index.js"></script>       
+</body>
+</html>
