5ef70b569445d0114b216517ab6ffd3d50046f6b
Julien Bruguet <lineproex@gmail.com>
2019-02-06 09:24:42 +0100

    Few changes on controllers & database connection
    From bindValue to execute(array()) in controllers & removed an useless line in database_inc

--- controler/addEvent.php
+++ controler/addEvent.php
@@ -9,13 +9,13 @@ $end = $_POST["end"];
 try {
 	// Preparing the insert/add request
    	$request = $db->prepare("INSERT INTO Events (EventTitre, EventMessage, EventDateDebut, EventDateFin) VALUES (:title, :message, :start, :end)");
-   	// Binding values
-	$request->bindValue(":title", $title);
-	$request->bindValue(":message", $message);
-	$request->bindValue(":start", $start);
-	$request->bindValue(":end", $end);
-	// Firing the request
-	$request->execute();
+	// Firing the request with some values binding
+	$request->execute(array(
+		':title' => $title,
+		':message' => $message,
+		':start' => $start,
+		':end' => $end
+	));
 } catch (Exception $e) { // If there was an error while deleting
     throw "Error when adding: ".$e; // Display it
 }
--- controler/deleteEvent.php
+++ controler/deleteEvent.php
@@ -6,10 +6,10 @@ $id = $_POST["id"];
 try {
 	// Preparing the request to delete the event
     $request = $db->prepare("DELETE FROM Events WHERE EventId = :id");
-    // Binding the ID to the request
-    $request->bindValue(":id", $id);
-    // Firing the request
-	$request->execute();
+    // Firing the request with the ID binded
+	$request->execute(array(
+		':id' => $id
+	));
 } catch (Exception $e) { // If there was an error while deleting
 	throw "Error when deleting: ".$e; // Display it
 }
