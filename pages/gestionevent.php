905e6b850996f0d5647dbe08f10786a5b2509432
Florient <flo.roku@gmail.com>
2019-02-07 09:01:35 +0100

    correction image

--- pages/gestionevent.php
+++ pages/gestionevent.php
@@ -50,6 +50,19 @@
             $('.content').richText();
             $('.content2').richText();
         });
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+        
+                reader.onload = function (e) {
+                    $('#picture')
+                        .attr('src', e.target.result);
+                };
+        
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
     </script>
 
 
--- scripts/gestionevent.js
+++ scripts/gestionevent.js
@@ -54,24 +54,6 @@ $(function() {
                 }
             }); 
 
-
         }
 
-
-
     }
-
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-    
-            reader.onload = function (e) {
-                $('#picture')
-                    .attr('src', e.target.result);
-            };
-    
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-});
