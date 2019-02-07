f9d52487fc0daca38d9c613e9e5f7d00341a2a45
maxence.sailly <maxencesai@gmail.com>
2019-02-07 11:16:34 +0100

    Ajout gestion photo

--- scripts/gestionevent.js
+++ scripts/gestionevent.js
@@ -1,60 +1,30 @@
 $(function () {
-    var add = document.getElementById("addEvent");
+    $("#form").on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+               url: "../controler/addEvent.php",
+         type: "POST",
+         data:  new FormData(this),
+         contentType: false,
+               cache: false,
+         processData:false,
+         success: function(data)
+            {
+           // view uploaded file.
+           $("#preview").html(data).fadeIn();
+           $("#form")[0].reset(); 
+           var resetDescription = document.getElementById("description");
+           resetDescription.innerHTML = 0;
+           var picture = document.getElementById("picture");
+           picture.setAttribute("src","http://placehold.it/180");
+            }          
+          });
+       });
+      
 
-    add.addEventListener("click", addNewEvent);
+    });
 
-    function addNewEvent() {
-        console.log("clique");
-        var picture = document.getElementById("pictureToGet");
-        var title = document.getElementById("titreevent");
-        var dateDeb = document.getElementById("datedeb");
-        var dateFin = document.getElementById("datefin");
-        var description = document.getElementById("description");
 
-        console.log(dateDeb);
-        var titleElement = title.value;
-        var dateDebElement = dateDeb.value;
-        var dateFinElement = dateFin.value;
-        var descriptionElement = description.value;
 
-        console.log(dateDebElement);
-        if (titleElement == "" || dateDebElement == "" || dateFinElement == "" || descriptionElement == "") {
-            if (titleElement == "") {
 
-            }
-            if (dateDebElement == "") {
 
-            }
-            if (dateFinElement == "") {
-
-            }
-            if (descriptionElement == "") {
-
-            }
-        } else {
-            console.log(new Date());
-
-            dateDebElement = moment(dateDebElement).format('YYYY-MM-DD');
-            dateFinElement = moment(dateFinElement).format('YYYY-MM-DD');
-
-            //            var data = [titleElement,descriptionElement,dateDebElement,dateFinElement];
-
-            $.ajax({
-                type: "POST",
-                data: {
-                    title: titleElement,
-                    message: descriptionElement,
-                    start: dateDebElement,
-                    end: dateFinElement
-                },
-                url: "../controler/addEvent.php",
-                dataType: "json",
-                success: function (data) {
-
-                }
-            });
-
-        }
-
-    }
-});
