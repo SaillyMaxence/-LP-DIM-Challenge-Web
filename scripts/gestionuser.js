32931272c372b32db194130faff66064b3567579
Julien Bruguet <lineproex@gmail.com>
2019-02-07 12:52:24 +0100

    Vérifier si l'utilisateur est un professeur sur l'ajout d'users

--- scripts/gestionuser.js
+++ scripts/gestionuser.js
@@ -28,7 +28,6 @@ $(function () {
         if (usernameElement == "") {
 
         } else {
-            //            var data = [username];
             $.ajax({
                 type: "POST",
                 data: {
@@ -37,9 +36,44 @@ $(function () {
                 url: "../controler/addUser.php",
                 dataType: "json",
                 success: function (data) {
-
+                    if(data == 0) { // If it is a teacher
+                        window.location = "user.php"; // Go to the users page
+                    } else if (data == 1) { // If not
+                        $("#err").css("color", "red"); // Set the error message's colour to red
+                        $("#err").text("Ce n'est pas un professeur !"); // Message "Not a teacher"
+                    } else if (data == 2) { // If it's already in the database
+                        $("#err").css("color", "red"); // Red text
+                        $("#err").text("L'utilisateur a déjà été ajouté"); // Message "Exists already"
+                    }
                 }
             });
         }
     }
+
+    $("#btncheck").click(function() {
+        // Récup de l'username
+        var username = document.getElementById("username");
+        var usernameElement = username.value;
+
+        $.ajax({
+            type: "POST",
+            data: {
+                username: usernameElement
+            },
+            url: "../controler/checkUser.php",
+            dataType: "json",
+            success: function(data) {
+                if(data == 0) { // If it's a teacher
+                    $("#err").css("color", "green"); // Green text
+                    $("#err").text("Valide !"); // Message "VALID"
+                } else if (data == 1) { // If not
+                    $("#err").css("color", "red"); // Set the error message's colour to red
+                    $("#err").text("Ce n'est pas un professeur !"); // Message "Not a teacher"
+                } else if (data == 2) { // If it's already in the database
+                    $("#err").css("color", "red"); // Red text
+                    $("#err").text("L'utilisateur a déjà été ajouté"); // Message "Exists already"
+                }
+            }
+        });
+    });
 });
