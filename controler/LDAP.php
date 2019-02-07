3b9babaf0f72156fd3cfea32bd80b511acc6aefe
vanackere.jeremy <jeremy.vanackere@outlook.fr>
2019-02-07 10:39:06 +0100

    nouvel fonction LDAP qui permet de vérifier si un user est un enseignant

--- controler/LDAP.php
+++ controler/LDAP.php
@@ -34,6 +34,20 @@
                 return false;
         }
 		
+		public function VerifGroupEnseigant($user)
+		{
+			if($this->_con)
+            {
+				    $basedn = "ou=Groups,dc=iutcb,dc=univ-littoral,dc=fr";
+                    $filter = "(&(memberuid=".$user.")(cn=enseigant))";
+                    $recherchegroupe = ldap_search($this->_con, $basedn, $filter,array("memberuid"));
+                    $resultatgroupe = ldap_get_entries($this->_con,$recherchegroupe);
+                    if($resultatgroupe['count'] > 0)
+                        return true;
+                    else
+                        return false;
+			}
+		}
 
         public function VerifUserGroup($user,$password,$group)
         {
