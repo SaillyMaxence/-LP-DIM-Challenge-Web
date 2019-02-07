4b437385f501b316ddc98e3e02a117d519e8e9bd
vanackere.jeremy <jeremy.vanackere@outlook.fr>
2019-02-07 12:51:52 +0100

    MAJ setConnexion

--- controler/setConnexion.php
+++ controler/setConnexion.php
@@ -16,7 +16,7 @@ if(isset($_POST["login"]) && isset($_POST["pass"])){
     if($con) // If connexion Server ldap is true
     {
         /// if user exist with this password AND verify if user belongs of a specific group
-        if($ldap->VerifUserGroup($loginUser,$passwordUser,"lpdim") || $ldap->VerifUserGroup($loginUser,$passwordUser,"enseignant")) 
+        if($ldap->VerifUserGroup($loginUser,$passwordUser,"enseignant") ||$ldap->VerifUserGroup($loginUser,$passwordUser,"lpdim")) 
         {
             // The query requete SQL, SELECT user where userName is user login
             $query= "SELECT * FROM users WHERE UserName=:UserName"; 
